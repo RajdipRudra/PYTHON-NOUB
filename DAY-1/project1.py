ba2dbce03b5c8fc3d6d7f9f72280213f2b318b63
rudra <rajdiprudra100@gmail.com>
2026-05-05 18:38:48 +0600

    day_1 Done

---
+++ DAY-1/project1.py
@@ -0,0 +1,42 @@
+logo = """
+XXXXXXXXXXX         X           XXX         X    XXXXXXXX                   
+XXX     XXXX       XXX          XXXX       XXX  XXXXXXXXXXX                 
+XXX     XXXX      XXXXX         XXXXX      XXX  XXXXXXXXXXXXX               
+XXX     XXXX      XXXXXX        XXXXXX     XXX  XXX     XXXXXXX             
+XXX     XXX       XXXXXXX       XXXXXXX    XXX  XXXX      XXXXXX            
+XXX     XXX      XXXX XXXX      XXX XXXX   XXX  XXXX        XXXX            
+XXXXXXXXXXXX     XXX   XXXX     XXX  XXXX  XXX  XXX          XXXX           
+XXXXXXXXXXXXX   XXXX    XXXX    XXX   XXXX XXX  XXX           XXX           
+XXX       XXXX  XXXXXXXXXXXXX   XXX    XXXXXXXX XXX          XXXX           
+XXX        XXX  XXXXXXXXXXXXXX XXXX     XXXXXXX XXX         XXXX            
+XXX        XXXXXXXXXXXXXXX XXXXXXX       XXXXXX XXX       XXXXX             
+XXX       XXXXXXXX          XXXXXX        XXXXX XXX     XXXXXX              
+XXX      XXXXX XXX           XXXXX         XXXX XXX  XXXXXXXX               
+XXXXXXXXXXXXX XXXX             XXX          XXX XXXXXXXXXXX                 
+XXXXXXXXXXX   XXX             XXXX           X  XXXXXXXXX                   
+ XXXXX         X              XXX               XXXXXX                      
+                               X                 X      XXXXXXXXXXXXXXX     
+               X                        X               XXXXXXXXXXXXXXXXXXX 
+ X            XXX                      XXX       X      XXXXXXXXXXXXXXXX    
+XXXX         XXXX         X           XXXX      XXX     XXXX                
+XXXXX       XXXX         XXX          XXXXX   XXXXX     XXXXXXXXXX          
+XXXXXXX     XXXX        XXXX          XXXXXX XXXXXX     XXXXXXXXXXXX        
+XXXXXXXX   XXXX        XXXXXX         XXXXXX XXXXX      XXXXXXXXXXX         
+XXX XXXXXX XXXX       XXX  XXX       XXX XXXXXXXXXX     XXXX                
+XXX   XXXXXXXXX      XXX   XXXX      XXX  XXXXX XXX     XXXX                
+XXX    XXXXXXXX     XXX      XXX    XXX    XXX  XXX     XXXX                
+XX       XXXXXXX   XXXXXXXXXXXXXX   XXX     X   XXX     XXXX                
+XX        XXXXXX  XXXXXXXXXXXXXXXX  XXX         XXXX    XXXX                
+XX         XXXXX XXXX          XXXXXXXX          XXXX   XXXXXXXXXXXXXXXXX   
+XX          XXXXXXXX            XXXXXX            XXX   XXXXXXXXXXXXXXXXXXXX
+X            XX  XX              XXX               X     XXXXXXXXXXXXXXX    
+                                  X                                         
+"""
+
+print(logo)
+
+print("WELLCOME TO BAND NAME GEN")
+city_name = input("what's the name of your city? ")
+pet_name = input("what's the name of your pet? ")
+
+print(f"The name of YOur Band is {city_name}-{pet_name}")
