fc1c238725747ebe7773a14979a54256222fa9c2
rudra <rajdiprudra100@gmail.com>
2026-06-06 17:26:15 +0600

    better version day_10

--- DAY-10/Calc.py
+++ DAY-10/Calc.py
@@ -12,23 +12,26 @@ def divition(a,b):
     return a/b
 
 
+operationss = {
+    "+":addition,
+    "-":substraction,
+    "*":multi,
+    "/":divition
+}
+
+
 first_num = int(input("What's the first num?: "))
 while True:
     operation = input("+\n-\n*\n/\npick an operation: ")
     sec_num = int(input("what's the next number?: "))
-
-    if operation=="+":
-        output = addition(first_num,sec_num)
+    if operation in operationss:
+        output = operationss[operation](first_num,sec_num)
         print(f"{first_num}{operation}{sec_num} = {output}")
-    elif operation=="-":
-        output = substraction(first_num,sec_num)
+    else:
+        operation = input("+\n-\n*\n/\npick an operation: ")
+        output = operationss[operation](first_num,sec_num)
         print(f"{first_num}{operation}{sec_num} = {output}")
-    elif operation=="*":
-        output = multi(first_num,sec_num)
-        print(f"{first_num}{operation}{sec_num} = {output}")
-    elif operation=="/":
-        output = divition(first_num,sec_num)
-        print(f"{first_num}{operation}{sec_num} = {output}")
+        
     contiNuee = input("do u wanna continue with the output? y or n").lower()
     if contiNuee=="y":
         first_num = output
